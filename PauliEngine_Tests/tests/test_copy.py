be543099fa6a906663d30f0999a135d11890503d
J. S. Kottmann <jakob.kottmann@gmail.com>
2025-12-08 21:49:52 +0100

    small cleanup

--- PauliEngine_Tests/tests/test_copy.py
+++
@@ -1,8 +0,0 @@
-import PauliEngine
-import generator
-
-
-def test_copy():
-        ps = PauliEngine.PauliString(generator.generate_paulistring(20), 1)
-        ps_copy = ps.copy()
-        assert(ps_copy.equals(ps))
